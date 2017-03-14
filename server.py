4883ecccefc92dd1e24173159c5b5eb81677d3d0
Peng Fei Wang <peng@Pengs-MacBook-Pro.local>
2017-03-14 18:56:54 -0400

    connected to firebase

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from firebase import firebase
 import SocketServer
+
+firebase = firebase.FirebaseApplication('https://ais-accounting.firebaseio.com/', None)
+new_user = 'Name:Peng'
+
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -16,9 +21,12 @@ class Handler(BaseHTTPRequestHandler):
         self._set_headers()
         
     def do_POST(self):
+    	global firebase
         content_length = int(self.headers.getheader('content-length', 0))
         post_data = self.rfile.read(content_length)
         print post_data
+        result = firebase.post('/ais-accounting/users', post_data)
+        print result
         #self._set_headers()
         #self.wfile.write("<html><body><h1>POST!</h1></body></html>")
         
